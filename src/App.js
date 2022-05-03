c723e9a99cda60bac18b48ec40d483781b9391b8
Matthew Rhoads <mematthew123@gmail.com>
2022-05-02 18:47:19 -0600

    created components and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom"
+import React from "react"
+import Home from "./components/Home"
+import About from "./components/About"
+import NavBar from "./components/NavBar"
+import Projects from "./components/Projects"
+import Post from "./components/Post"
+import SinglePost from "./components/SinglePost"
 
-function App() {
+function App() {  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <BrowserRouter>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/navBar" component={NavBar} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/post/:slug" component={Post} />
+      <Route path="/singlepost" component={SinglePost} />
+
+    </BrowserRouter>
   );
 }
 
-export default App;
+
+export default App
---
+++ src/components/SinglePost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SinglePost() {
+  return (
+    <div>singlepost</div>
+  )
+}
+
+export default SinglePost
