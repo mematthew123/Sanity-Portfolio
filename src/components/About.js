83e4b44bafbb5be57547da2ccf72d7c0cc7b67ee
Matthew Rhoads <mematthew123@gmail.com>
2022-05-13 17:39:29 -0600

    added test fields to about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,22 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
-        quisquam.
-      </p>
+    <main className="container mx-auto bg-black text-white min-h-screen min-w-full pt-10 ">
+    <div className="  pt-2 text-center w-1/3 bg-blue-500 text-white rounded">
+      <h1>About- Personal </h1>
+  
     </div>
+
+    <div className=" bg-blue-500 absolute top-1/3 right-0 h-16 w-1/3 rounded ...">
+      <h1>Skills</h1>
+</div>
+      <div className=" bg-blue-500 absolute top-2/3 left-0 h-16 w-1/3 rounded ...">
+      <h1>Bernice</h1>
+      </div>
+  <div className="absolute inset-x-0 bottom-0 h-12 text-center bg-blue-500 ...">
+<h1>contact </h1>
+
+  </div>
+    </main>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,33 @@ import React from "react";
 
   export default function Home() {
     return (
-      
-      <div className="  container mx-auto  bg-black text-white min-h-screen min-w-full">
-          <div className=" mt-0.5 text-5xl text-white flex justify-center cursive">
-
-        <h1 className="text-5xl mt-40 sm:mt-10 text-white flex justify-center cursive">Matthew Rhoads</h1>
-
-
-          </div>
 
 
 
+<main className="container mx-auto bg-black text-white min-h-screen min-w-full">
+  <div className="flex flex-col md:flex-row justify-center p-6 ">
+    <div className=" justify-center">
+      <h1 className="text-5xl font-bold leading-none mb-2 justify-center mt-32 sm:mt-20">
+        Hello, I'm <span className="text-blue-500">Matthew</span>
+      </h1>
+      <p className="text-xl">
+        I'm a Full Stack Developer.
+      </p>
+      <p className="text-xl">
+Missoula, MT      </p>
+      <p className="text-xl">
+Thanks for checkingout my site      </p>
+      <p className="text-xl">
+Live, Laugh, Love      </p>
       </div>
-    );
+      </div>
+      </main>
+
+
+
+      
+//       
+
+
+                );
   }
