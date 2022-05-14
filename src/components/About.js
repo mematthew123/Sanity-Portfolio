7119989052571656377845ac224cc750df5c3e1f
Matthew Rhoads <mematthew123@gmail.com>
2022-05-13 23:12:14 -0600

    updated about  me

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,14 @@ export default function About() {
       <div className="lg:w-4/12 flex justify-center items-center">
         <div>
           <h1 className="dark:text-white text-4xl md:text-5xl xl:text-6xl font-semibold text-white w-7/12">Who am i ...?</h1>
-          <p className="dark:text-gray-300 md:w-7/12 lg:w-11/12 xl:w-10/12 mt-4 lg:mt-5 text-base leading-normal text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.</p>
+          <p className="dark:text-gray-300 md:w-7/12 lg:w-11/12 xl:w-10/12 mt-4 lg:mt-5 text-base leading-normal text-white">
+          I am skilled in Javascript, Java, Reactjs, Shopify and Wordpress. I take  pride in my work and always strive to deliver a high quality product.
+ I have a passion for learning new things and I am always looking to improve my skills.
+</p>
+            <p className="dark:text-gray-300 md:w-7/12 lg:w-11/12 xl:w-10/12 mt-4 lg:mt-5 text-base leading-normal text-white">
+              When not in front of a computer, you can usually find me spenidng time along the Clark Fork river or in the mountains.
+              </p>
+            
         </div>
       </div>
       <div className="lg:w-8/12 mt-6 md:mt-8 lg:mt-0">
