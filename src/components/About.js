eb7ba7ae1f43b0b854be321234670f861ddad481
Matthew Rhoads <mematthew123@gmail.com>
2022-05-13 22:01:43 -0600

    UPDATED ABOUT ME

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,32 @@
 import React from "react";
+import image from "/Users/matthew/Documents/GitHub/Sanity-Portfolio/src/guy.jpg";
+
+
 
 export default function About() {
+
   return (
-    <main className="container mx-auto bg-black text-white min-h-screen min-w-full pt-10 ">
-    <div className="  pt-2 text-center w-1/3 bg-blue-500 text-white rounded">
-      <h1>About- Personal </h1>
-  
-    </div>
-
-    <div className=" bg-blue-500 absolute top-1/3 right-0 h-16 w-1/3 rounded ...">
-      <h1>Skills</h1>
-</div>
-      <div className=" bg-blue-500 absolute top-2/3 left-0 h-16 w-1/3 rounded ...">
-      <h1>Bernice</h1>
+<div className="bg-black min-h-screen p-12">
+<div className="dark:bg-gray-900">
+  <div className="container mx-auto py-9 md:py-12 lg:py-24">
+    <div className="flex flex-col lg:flex-row justify-center items-strech mx-4">
+      <div className="lg:w-4/12 flex justify-center items-center">
+        <div>
+          <h1 className="dark:text-white text-4xl md:text-5xl xl:text-6xl font-semibold text-white w-7/12">Who am i ...?</h1>
+          <p className="dark:text-gray-300 md:w-7/12 lg:w-11/12 xl:w-10/12 mt-4 lg:mt-5 text-base leading-normal text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.</p>
+        </div>
       </div>
-  <div className="absolute inset-x-0 bottom-0 h-12 text-center bg-blue-500 ...">
-<h1>contact </h1>
-
+      <div className="lg:w-8/12 mt-6 md:mt-8 lg:mt-0">
+        <div className="relative w-full h-full">
+          <img src={image}className="w-full h-full relative hidden lg:block" />
+          <img src={image} className="w-full h-full lg:hidden sm:hidden" />
+           
+          </div>
+      
+      </div>
+    </div>
   </div>
-    </main>
+  </div>
+</div>
   );
 }
